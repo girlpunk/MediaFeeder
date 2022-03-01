11e0bc1924e65972bf0c899046ea49464a5e389a
Foxocube <me@foxocube.xyz>
2022-03-01 21:50:32 +0000

    add stats API endpoint

---
+++ MediaFeeder/Controllers/StatsController.cs
@@ -0,0 +1,56 @@
+﻿using MediaFeeder.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MediaFeeder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatsController : ControllerBase
+    {
+        private IDbContextFactory<MediaFeederDataContext> _contextFactory { get; set; }
+
+        public StatsController(IDbContextFactory<MediaFeederDataContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        // GET: api/<StatsController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using var db = await _contextFactory.CreateDbContextAsync();
+
+            var lastHour = DateTime.UtcNow - TimeSpan.FromHours(1);
+            var new_published = await db.YtManagerAppVideos.CountAsync(video => video.PublishDate >= lastHour);
+            
+            var subscriptions = await db.YtManagerAppSubscriptions.CountAsync();
+            var tracked_videos = await db.YtManagerAppVideos.CountAsync();
+            var watched_videos = await db.YtManagerAppVideos.CountAsync(video => video.Watched);
+            var new_unwatched = await db.YtManagerAppVideos.CountAsync(video => !video.Watched);
+
+            var folders = await db.YtManagerAppVideos
+                .GroupBy(video => video.Subscription.ParentFolderId)
+                .Select(group => new {
+                    Key = group.Key ?? -1,
+                    Name = group.First().Subscription.ParentFolder.Name,
+                    Tracked = group.Count(),
+                    UnwatchedCount = group.Count(video => !video.Watched),
+                    UnwatchedDuration = group.Sum(video => video.Duration)
+                })
+                .ToDictionaryAsync(group => group.Key, group => group);
+
+            return Ok(new
+            {
+                Published = new_published,
+                Subscriptions = subscriptions,
+                VideosTracked = tracked_videos,
+                VideosWatched = watched_videos,
+                VideosUnwatched = new_unwatched,
+                Folders = folders
+            });
+        }
+    }
+}
--- MediaFeeder/Data/MediaFeederDataContext.cs
+++ MediaFeeder/Data/MediaFeederDataContext.cs
@@ -44,7 +44,7 @@ namespace MediaFeeder.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=helix;Database=ytsm;Username=ytsm;Password=verysecurepassword;port=5432");
+                optionsBuilder.UseNpgsql("Host=localhost;Database=ytsm;Username=ytsm;Password=verysecurepassword;port=5432");
             }
         }
 
