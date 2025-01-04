b8440345046562ebff3046c1dc3224780604a773
Foxocube <git@foxocube.xyz>
2025-01-04 16:59:33 +0000

    Add more debug hooks for experimental downloaded video frame

--- MediaFeeder/MediaFeeder/Providers/DownloadedVideoFrame.razor.cs
+++ MediaFeeder/MediaFeeder/Providers/DownloadedVideoFrame.razor.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using Blazored.Video;
 using Blazored.Video.Support;
 
 namespace MediaFeeder.Providers;
@@ -6,16 +8,40 @@ public partial class DownloadedVideoFrame
 {
     private void CanPlay(VideoState obj)
     {
-        throw new NotImplementedException();
+        Console.WriteLine($"CanPlay {JsonSerializer.Serialize(obj)}");
+
+        Player?.Play(obj);
     }
 
     private void Ended(VideoState obj)
     {
-        throw new NotImplementedException();
+        Console.WriteLine($"Ended {JsonSerializer.Serialize(obj)}");
+
+        if (Page != null)
+            Task.Run(async () => await Page.GoNext(true));
     }
 
     private void CanPlayThrough(VideoState obj)
     {
-        throw new NotImplementedException();
+        Console.WriteLine($"CanPlayThrough {JsonSerializer.Serialize(obj)}");
+
+        Player?.Play(obj);
+    }
+
+    public BlazoredVideo? Player { get; set; }
+
+    private void Abort(VideoState obj)
+    {
+        Console.WriteLine($"Abort {JsonSerializer.Serialize(obj)}");
+    }
+
+    private void Error(VideoState obj)
+    {
+        Console.WriteLine($"Error {JsonSerializer.Serialize(obj)}");
+    }
+
+    private void LoadedData(VideoState obj)
+    {
+        Console.WriteLine($"LoadedData {JsonSerializer.Serialize(obj)}");
     }
 }
