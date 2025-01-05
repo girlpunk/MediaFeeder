967414cffa45fcee1ed3fd713c4df4a905507d5f
Foxocube <git@foxocube.xyz>
2025-01-05 14:18:00 +0000

    Fix DB loading

--- MediaFeeder/MediaFeeder/Data/db/Subscription.cs
+++ MediaFeeder/MediaFeeder/Data/db/Subscription.cs
@@ -4,20 +4,15 @@ namespace MediaFeeder.Data.db;
 
 public class Subscription : ITreeSelectable
 {
-    public Subscription()
-    {
-        Videos = new HashSet<Video>();
-    }
-
     public int Id { get; set; }
 
-    [MaxLength(1024)] public string Name { get; set; }
+    [MaxLength(1024)] public required string Name { get; set; }
 
-    [MaxLength(128)] public string PlaylistId { get; set; }
+    [MaxLength(128)] public required string PlaylistId { get; set; }
 
-    public string Description { get; set; }
+    [MaxLength(1000000000)] public required string Description { get; set; }
 
-    [MaxLength(1024)] public string Thumbnail { get; set; }
+    [MaxLength(1024)] public required string Thumbnail { get; set; }
 
     public bool? AutoDownload { get; set; }
     public int? DownloadLimit { get; set; }
@@ -28,20 +23,21 @@ public class Subscription : ITreeSelectable
     public int? ParentFolderId { get; set; }
     public int UserId { get; set; }
 
-    [MaxLength(128)] public string ChannelId { get; set; }
+    [MaxLength(128)] public required string ChannelId { get; set; }
 
-    [MaxLength(1024)] public string ChannelName { get; set; }
+    [MaxLength(1024)] public required string ChannelName { get; set; }
 
     public bool RewritePlaylistIndices { get; set; }
-    public DateTime? LastSynchronised { get; set; }
+    public DateTimeOffset? LastSynchronised { get; set; }
 
-    [MaxLength(64)] public string Provider { get; set; }
+    [MaxLength(64)] public required string Provider { get; set; }
 
     [MaxLength(100)] public string? Thumb { get; set; }
 
-    public virtual Folder ParentFolder { get; set; }
-    public virtual AuthUser User { get; set; }
-    public virtual ICollection<Video> Videos { get; init; }
+
+    public virtual Folder? ParentFolder { get; set; }
+    public virtual AuthUser User { get; set; } = null!;
+    public virtual ICollection<Video> Videos { get; init; } = null!;
 
     public string OnSelectedNavigate => "/subscription/" + Id;
 }
--- MediaFeeder/MediaFeeder/Data/db/Video.cs
+++ MediaFeeder/MediaFeeder/Data/db/Video.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MediaFeeder.Data.db;
 
@@ -6,19 +7,19 @@ public class Video
 {
     public int Id { get; set; }
 
-    [MaxLength(12)] public string VideoId { get; set; }
+    [MaxLength(12)] public required string VideoId { get; set; }
 
-    public string Name { get; set; }
-    public string Description { get; set; }
+    [MaxLength(1000000000)] public required string Name { get; set; }
+    [MaxLength(1000000000)] public required string Description { get; set; }
     public bool Watched { get; set; }
-    public string? DownloadedPath { get; set; }
+    [MaxLength(1000000000)] public string? DownloadedPath { get; set; }
     public int PlaylistIndex { get; set; }
     public DateTimeOffset? PublishDate { get; set; }
-    public string Thumbnail { get; set; }
+    [MaxLength(1000000000)] public required string Thumbnail { get; set; }
     public int SubscriptionId { get; set; }
     public double Rating { get; set; }
 
-    [MaxLength(255)] public string UploaderName { get; set; }
+    [MaxLength(255)] public required string UploaderName { get; set; }
 
     public int Views { get; set; }
     public bool New { get; set; }
@@ -26,6 +27,7 @@ public class Video
 
     [MaxLength(100)] public string? Thumb { get; set; }
 
+    [NotMapped]
     public TimeSpan DurationSpan
     {
         get => TimeSpan.FromSeconds(Duration);
