506c8c0aff5a146501d07d698f8c1fbe1aa1d9e2
Foxocube <git@foxocube.xyz>
2025-04-08 12:56:54 +0100

    Try to fix not loading

--- MediaFeeder/MediaFeeder/Components/Tree/TreeView.razor.cs
+++ MediaFeeder/MediaFeeder/Components/Tree/TreeView.razor.cs
@@ -30,78 +30,75 @@ public sealed partial class TreeView
     private SemaphoreSlim Updating { get; } = new(1);
     private Tree<ITreeSelectable>? TreeRef { get; set; }
 
-    protected override async Task OnParametersSetAsync()
+    protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        if (UnwatchedCache == null && Folders == null)
-            Update();
-
-        await base.OnParametersSetAsync();
-    }
-
-    private async void Update()
-    {
-        try
+        if (firstRender)
         {
-            Logger.LogInformation("Waiting for lock");
-            await Updating.WaitAsync();
-
-            Logger.LogInformation("Got lock, checking auth");
-            var auth = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-            var user = await UserManager.GetUserAsync(auth.User);
-
-            ArgumentNullException.ThrowIfNull(user);
-
-            Logger.LogInformation("Preparing queries");
-            await using var context = await ContextFactory.CreateDbContextAsync();
-
-            Logger.LogInformation("Staring unwatched");
-            var unwatched = await context.Videos
-                .Where(v => !v.Watched && v.Subscription!.UserId == user.Id)
-                .GroupBy(static v => v.SubscriptionId)
-                .ToDictionaryAsync(static g => g.Key, static g => g.Count());
-
-            Logger.LogInformation("Unwatched done, starting downloaded");
-            var downloaded = await context.Videos.Where(v => v.Subscription!.UserId == user.Id)
-                .GroupBy(static v => v.SubscriptionId)
-                .ToDictionaryAsync(static g => g.Key, static g => g.Count());
-
-            Logger.LogInformation("downloaded done, starting merge");
-            var cache = new Dictionary<int, (int unwatched, int downloaded)>(int.Max(unwatched.Count, downloaded.Count));
-
-            Logger.LogInformation("merge step 1");
-            foreach (var pair in unwatched)
-                cache.Add(pair.Key, (unwatched: pair.Value, downloaded: 0));
-
-            Logger.LogInformation("merge step 2");
-            foreach (var pair in downloaded)
+            try
             {
-                if (cache.ContainsKey(pair.Key))
-                    cache[pair.Key] = (cache[pair.Key].unwatched, pair.Value);
-                else
-                    cache.Add(pair.Key, (unwatched: 0, downloaded: pair.Value));
+                Logger.LogInformation("Waiting for lock");
+                await Updating.WaitAsync();
+
+                Logger.LogInformation("Got lock, checking auth");
+                var auth = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+                var user = await UserManager.GetUserAsync(auth.User);
+
+                ArgumentNullException.ThrowIfNull(user);
+
+                Logger.LogInformation("Preparing queries");
+                await using var context = await ContextFactory.CreateDbContextAsync();
+
+                Logger.LogInformation("Staring unwatched");
+                var unwatched = await context.Videos
+                    .Where(v => !v.Watched && v.Subscription!.UserId == user.Id)
+                    .GroupBy(static v => v.SubscriptionId)
+                    .ToDictionaryAsync(static g => g.Key, static g => g.Count());
+
+                Logger.LogInformation("Unwatched done, starting downloaded");
+                var downloaded = await context.Videos.Where(v => v.Subscription!.UserId == user.Id)
+                    .GroupBy(static v => v.SubscriptionId)
+                    .ToDictionaryAsync(static g => g.Key, static g => g.Count());
+
+                Logger.LogInformation("downloaded done, starting merge");
+                var cache = new Dictionary<int, (int unwatched, int downloaded)>(int.Max(unwatched.Count,
+                    downloaded.Count));
+
+                Logger.LogInformation("merge step 1");
+                foreach (var pair in unwatched)
+                    cache.Add(pair.Key, (unwatched: pair.Value, downloaded: 0));
+
+                Logger.LogInformation("merge step 2");
+                foreach (var pair in downloaded)
+                {
+                    if (cache.ContainsKey(pair.Key))
+                        cache[pair.Key] = (cache[pair.Key].unwatched, pair.Value);
+                    else
+                        cache.Add(pair.Key, (unwatched: 0, downloaded: pair.Value));
+                }
+
+                Logger.LogInformation("merge step 3");
+                UnwatchedCache = cache;
+
+                Logger.LogInformation("merge done, getting folders");
+                Folders = await context.Folders.Where(f => f.UserId == user.Id && f.ParentId == null)
+                    .Include(static f => f.Subfolders)
+                    .Include(static f => f.Subscriptions)
+                    .ToListAsync();
+
+                // Logger.LogInformation("Filtering fonder");
+                // adding this filter to above query fails with an error about lazy-load after the DbContext was disposed.
+                // Folders = Folders.Where(static f => f.ParentId == null).ToList();
+
+                Logger.LogInformation("Done!");
+                await InvokeAsync(StateHasChanged);
             }
-
-            Logger.LogInformation("merge step 3");
-            UnwatchedCache = cache;
-
-            Logger.LogInformation("merge done, getting folders");
-            Folders = await context.Folders.Where(f => f.UserId == user.Id && f.ParentId == null)
-                .Include(static f => f.Subfolders)
-                .Include(static f => f.Subscriptions)
-                .ToListAsync();
-
-            // Logger.LogInformation("Filtering fonder");
-            // adding this filter to above query fails with an error about lazy-load after the DbContext was disposed.
-            // Folders = Folders.Where(static f => f.ParentId == null).ToList();
-
-            Logger.LogInformation("Done!");
-        }
-        finally
-        {
-            Updating.Release();
+            finally
+            {
+                Updating.Release();
+            }
         }
 
-        await InvokeAsync(StateHasChanged);
+        await base.OnAfterRenderAsync(firstRender);
     }
 
     private void EditFolder(Folder? folder)
