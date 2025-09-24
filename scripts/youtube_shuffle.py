f9cabb1d93741e42dc5b3ca73a2412bf99a8691b
haku <git@haku.me>
2025-09-24 10:31:59 +0000

    Pause after marking as watched so that it does not continue playing when skipping the last video in the queue.

--- scripts/youtube_shuffle.py
+++ scripts/youtube_shuffle.py
@@ -128,6 +128,10 @@ class MyMediaStatusListener(MediaStatusListener):
             self._logger.info("Received next video ID: %s", rep.NextVideoId)
             self.event_queue.put(QueueEvent(next_video_id=rep.NextVideoId))
 
+    def pause_if_playing(self):
+      if self.last_status.player_is_playing:
+        cast.media_controller.pause()
+        self._logger.info("paused")
 
 T = TypeVar("T")
 
@@ -312,6 +316,7 @@ try:
                 if current_video_id and event.mark_watched:
                     logger.info("Marking %s as watched...", current_video_id)
                     stub.Watched(Api_pb2.WatchedRequest(Id=current_video_id, Watched=True, ActuallyWatched=True))
+                    listener_media.pause_if_playing() # so playback stops if last video in queue
 
                 current_video_id = None
                 current_content_id = None
