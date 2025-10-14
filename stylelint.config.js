274f77754ecf03c16c48ae85041169c7d0640245
haku <git@haku.me>
2025-10-14 14:12:17 +0000

    Update CSS linter rules.

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,7 +3,9 @@ export default {
   extends: ["stylelint-config-standard"],
   // see https://stylelint.io/user-guide/rules
   rules: {
-    "selector-pseudo-element-no-unknown": [true, { ignorePseudoElements: ["deep"] }],
     "alpha-value-notation": "number",
+    "selector-pseudo-element-no-unknown": [true, { ignorePseudoElements: ["deep"] }],
+    'rule-empty-line-before': null,
+    'shorthand-property-no-redundant-values': null,
   },
 };
