b57cf9e9ad2859aac7bc7fc055e4b350282f70b3
haku <git@haku.me>
2025-11-11 22:13:37 +0000

    Read JWT auth config from Auth section instead of ApiAuth section.

--- MediaFeeder/Program.cs
+++ MediaFeeder/Program.cs
@@ -123,21 +123,7 @@ builder.Services.AddAuthentication()
         JwtBearerDefaults.AuthenticationScheme,
         options =>
         {
-            builder.Configuration.GetSection("ApiAuth").Bind(options);
-
-            //var selfIssuer = authSettings.GetValue<string>("issuer", "MediaFeeder");
-
-            //options.TokenValidationParameters.ValidIssuers = [selfIssuer];
-            //options.TokenValidationParameters.ValidAudience = selfIssuer;
-            //options.TokenValidationParameters.IssuerSigningKey =
-            //    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authSettings.GetValue<string>("secret") ??
-            //                                                    throw new InvalidOperationException()));
-
-            //options.TokenValidationParameters.ValidateLifetime = true;
-            //options.TokenValidationParameters.ValidateIssuerSigningKey = true;
-            //options.TokenValidationParameters.ValidateIssuer = true;
-            //options.TokenValidationParameters.ValidateAudience = true;
-
+            builder.Configuration.GetSection("Auth").Bind(options);
             options.EventsType = typeof(AppJwtBearerEvents);
         });
 
